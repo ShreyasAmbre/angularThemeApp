9c49f4ff93f4d56675da22157941d60fd1486484
Shreyas Shrikant Ambre <shreyasambre@monocept.com>
2024-04-23 16:18:04 +0530

    First COmmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemesService } from './service/themes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'theme-app';
+
+  constructor(private theme: ThemesService){}
+
+  ngOnInit(): void {
+    this.changeTheme('red')
+  }
+
+  changeTheme(themeName: string){
+    this.theme.loadTheme(themeName)
+  }
 }
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ThemesService } from 'src/app/service/themes.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  constructor(){}
+  
+}
